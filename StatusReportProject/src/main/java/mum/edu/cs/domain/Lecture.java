f8059bec610c086b51ab4f933f89d27a829a985c
michael0521 <yxm1014@gmail.com>
2016-03-06 16:55:55 -0600

    remove intelij project file

--- StatusReportProject/src/main/java/mum/edu/cs/domain/Lecture.java
+++ StatusReportProject/src/main/java/mum/edu/cs/domain/Lecture.java
@@ -5,5 +5,5 @@ import java.util.List;
 public class Lecture {
 	
 	private List<Student> students;
-
+	
 }
--- StatusReportProject/src/main/java/mum/edu/cs/domain/Report.java
+++ StatusReportProject/src/main/java/mum/edu/cs/domain/Report.java
@@ -2,9 +2,7 @@ package mum.edu.cs.domain;
 
 public class Report {
 	
-	
 	private float grade;
 	private String comment;
 	
-
 }
