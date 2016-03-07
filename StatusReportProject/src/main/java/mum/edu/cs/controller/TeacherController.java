192bc9bfef02270a7d1527ecf019937bc138bcb5
michael0521 <yxm1014@gmail.com>
2016-03-06 18:44:00 -0600

    Teacher: Add test pages.

--- StatusReportProject/src/main/java/mum/edu/cs/controller/TeacherController.java
+++ StatusReportProject/src/main/java/mum/edu/cs/controller/TeacherController.java
@@ -1,12 +1,33 @@
 package mum.edu.cs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import mum.edu.cs.service.TeacherService;
 
-@Controller(value="/teacher")
+@Controller
+@RequestMapping("/teacher")
 public class TeacherController {
 	
+	@Autowired
 	private TeacherService teacherService;
 	
+	@RequestMapping("/lectures")
+	public String listLectures(){
+		
+		return "teacher/lectures";
+	}
+	
+	@RequestMapping("/reports")
+	public String listReportsOfLecture(){
+		
+		return "teacher/reports";
+	}
+	
+	@RequestMapping("/grade")
+	public String getGradeForm(){
+		
+		return "teacher/gradeReport";
+	}
 }
--- StatusReportProject/src/main/java/mum/edu/cs/service/TeacherService.java
+++ StatusReportProject/src/main/java/mum/edu/cs/service/TeacherService.java
@@ -3,9 +3,13 @@ package mum.edu.cs.service;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
 import mum.edu.cs.dao.TeacherDAO;
 
+@Service
 public class TeacherService {
+	
 	
 	private TeacherDAO teacherDAO;
 	
